a13a9c909d97ceba7eff38226c4a864bbf29117f
Jack Burns <chugga151@gmail.com>
2020-03-31 21:15:21 +0100

    Fixed team page

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/avery-prototype/precache-manifest.7bb1e073f5a920336f780e4d15cec6b7.js"
+  "/avery-prototype/precache-manifest.a5ff988efa65d4c8286cc6351a6a7759.js"
 );
 
 self.addEventListener('message', (event) => {
