fdfee27c3b1eaed57437de6f58c2ae039d44ffc8
Jack Burns <chugga151@gmail.com>
2020-03-31 21:53:34 +0100

    fixed the three main pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/avery-prototype/precache-manifest.a5ff988efa65d4c8286cc6351a6a7759.js"
+  "/avery-prototype/precache-manifest.869b064e72812f0134279698d1e068d3.js"
 );
 
 self.addEventListener('message', (event) => {
